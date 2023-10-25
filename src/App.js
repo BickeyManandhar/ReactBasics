c69130d465e5c2fc14f142539031de69f542cfb1
BickeyManandhar <manandharbickey@gmail.com>
2023-10-25 17:36:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+//Props are just arguments that you pass in react components to later pass dynamic data when you call the component
+// const Person = (props)=>{
+//   return (
+//     <>
+//       <h1>First Name: {props.firstname}</h1>
+//       <h2>Last Name: {props.lastname}</h2>
+//       <h2>Age: {props.age}</h2>
+//     </>
+//   )
+// }
+
+
+
+const App= () =>{
+  // const name = "Bickey"; // below we are returning JSX and we can write any JS code inside {} 
+  // const isNameShowing = true;
+  const [counter , setCounter] = useState(0); //here we are de-structuring array and assigning useState(0). What this does is it sets the initial value of counter to 0 and we will use setCounter function to set the value of counter . React hooks must be called inside react function component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello, {isNameShowing ? name : 'Your name is not showing'}!!!</h1>
+      {name ? (
+      <>
+      <h1>{name}</h1>
+      </>
+      ) : (
+      <>
+      <h1>This is one element</h1>
+      <h2>This is another element</h2>
+      </>)} */
+      // you can pass the data inside {} or without {} if you do not need to evaluate the data
+      // <>
+      // <Person firstname={'Aakash'} lastname={'KC'} age={20}/>
+      // <Person firstname='Bickey' lastname={'Manandhar'} age={25}/>
+      // <Person firstname={'Aashish'} lastname={'Karanjit'} age={22}/>
+      // </>
+      <>
+        <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
+        <h2>{counter}</h2>
+        <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+      </>
+      }
     </div>
   );
 }
